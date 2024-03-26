8b6856dc78592a788ea114b936653c786e8ec635
archika <archikakasliwal31@gmail.com>
2024-03-26 13:54:55 +0530

    contactController file modified

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,6 +1,8 @@
 const asyncHandler=require('express-async-handler')
 const Contact=require('../models/contactModel')
-
+//@desc get list of all contacts
+//@route GET /api/contacts
+//@access private
 const getContacts=asyncHandler(async(req,res)=>{
     const contacts= await Contact.find({user_id:req.user.id});
     res.status(200).json(contacts)
